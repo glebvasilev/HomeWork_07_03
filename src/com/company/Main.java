e054630bd4c2e1c8ce41a8c685da4d1e531abcd2
chibatuz <chibatuz@gmail.com>
2020-12-18 00:56:35 +0400

    New_commit_03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,82 +2,74 @@ package com.company;
 
 import java.util.Date;
 
-class Act {
+class Form {
+    public int number;
+    public Date date;
+    public String[] goods;
+}
+
+class Act extends Form {
     /*
      *  Class serves for acts
      */
 
-    protected int number;
-    protected Date Date;
-    protected String[] goods = {"one","two"};
+    public int number;
+    public Date date;
+    public String[] goods = {"one","two"};
     static int count;
 
-    public Act(int number, Date Date, String[] goods) {
+    public Act(int number, Date date, String[] goods) {
         this.number = number;
-        this.Date = Date;
+        this.date = date;
         this.goods = goods;
         count++;
     }
 
-    public void setNumber(int number) {
-
-        this.number = number;
-    }
-    public void setDate(Date date) {
-
-        this.Date = Date;
-    }
-    public void setGoods(String[] goods) {
-
-        this.goods = goods;
-    }
     public static int getCount(){
-
         return count;
     }
 }
-class Agreement {
+
+class Agreement extends Form {
     /*
      *  Class serves for agreements
      */
 
-    protected int number;
-    protected Date Date;
-    protected String[] goods = {"one","two"};
+    public int number;
+    public Date date;
+    public String[] goods = {"one","two"};
     static int count;
 
-    public Agreement(int number, Date Date, String[] goods) {
+    public Agreement(int number, Date date, String[] goods) {
         this.number = number;
-        this.Date = Date;
+        this.date = date;
         this.goods = goods;
         count++;
     }
 
-    public void setNumber(int number) {
-
-        this.number = number;
-    }
-    public void setDate(Date date) {
-
-        this.Date = Date;
-    }
-    public void setGoods(String[] goods) {
-
-        this.goods = goods;
-    }
     public static int getCount(){
-
         return count;
     }
 }
 
-public class Main {
+class Converter {
+    public static Form convert(Form o) {
+        if (o instanceof Act) {
+            return new Agreement(o.number, o.date, o.goods);
+        } else {
+            return new Act(o.number, o.date, o.goods);
+        }
+    }
+}
 
+public class Main {
     public static void main(String[] args) {
         Date date = new Date();
         Act a = new Act(1, date, args);
         System.out.println("Now we have " + Act.getCount() + " act");
         Agreement b = new Agreement(1, date, args);
         System.out.println("Now we have " + Agreement.getCount() + " agreement");
+        System.out.println("Converted from Act to: " + Converter.convert(a).getClass().getName());
+        System.out.println("Converted from Agreement to: " + Converter.convert(b).getClass().getName());
     }
 }
